3c8b3b320bc6d509345c33f6c854c0fe7fe76cac
Delcros Axel <axel.delcros@gmail.com>
2019-08-01 01:29:59 +0200

    Add Archives feature + Upgrade Component management

---
+++ src/Archive.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Archive.scss';
+
+const Archive = ({ informations }) => {
+  return (
+    <div class="Archive">
+      <ul className="Archive__List">
+        {informations.map(info => (
+          <a href={`/?week=${info.order}`}>
+            <li className="Archive__link" key={`week-${info.order}}`}>
+              Week n°{info.order}
+            </li>
+          </a>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Archive;
