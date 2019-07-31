cd1c61f24b8ebaa6b76dcff327c62cfea519f330
Delcros Axel <axel.delcros@gmail.com>
2019-08-01 01:08:09 +0200

    Add config file / Favicons / Refacto Enhance CSS / Manage back

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import queryString from "query-string";
 import classnames from "classnames";
 
 import { getRandomSoccerImage } from "./utils/helpers.js";
+import Lines from "./Lines.js";
+import Header from "./Header";
 import Logo from "./logo-color.svg";
 import "./App.css";
 
@@ -21,47 +23,6 @@ const authorIntroductions = [
   "Noté par ",
   "Envoyé à l'impression par "
 ];
-
-// API <Lines>{lines}</Lines>, where lines is an array of strings
-const Lines = ({ children }) => {
-  const El = ({ children }) => <span className="LineItalic">{children}</span>;
-  let danglingItalic = false;
-  const theLines = children.reduce((acc, line) => {
-    const parts = line.split('"');
-    let theLine = "";
-    // either " at beginning or end
-    if (parts.length === 2) {
-      // " at beginning
-      if (parts[0] === "") {
-        danglingItalic = true;
-        theLine = <El>"{parts[1]}</El>;
-      }
-      // " at end
-      if (parts[1] === "") {
-        danglingItalic = false;
-        theLine = <El>{parts[0]}"</El>;
-      }
-    } else if (parts.length === 1) {
-      // no "
-      // but maybe italic because of DANGLING El
-      theLine = danglingItalic ? <El>{parts[0]}</El> : parts[0];
-    } else {
-      // multiple "
-      theLine = (
-        <>
-          {parts.map((part, i) => {
-            if ((i + 1) % 2 === 0) {
-              return <El>"{part}"</El>;
-            }
-            return part;
-          })}
-        </>
-      );
-    }
-    return [...acc, <p>{theLine}</p>];
-  }, []);
-  return <div className="Lines">{theLines}</div>;
-};
 
 const App = ({ location }) => {
   const [weekInformations, setWeekInformations] = useState({});
@@ -88,21 +49,14 @@ const App = ({ location }) => {
     getImage();
   }, [location.search, weekKey]);
 
-  useEffect(() => {
-    
-  }, [mainInformation]);
+  useEffect(() => {}, [mainInformation]);
 
   const readInfo = event => {
     event.preventDefault();
     setIsReading(true);
   };
 
-  const stopReading = event => {
-    event.preventDefault();
-    setIsReading(false);
-  };
-
-  const handleReadArticle = (article) => {
+  const handleReadArticle = article => {
     setMainInformation(article);
     setIsLoading(true);
     async function getImage() {
@@ -113,7 +67,11 @@ const App = ({ location }) => {
     }
     window.scrollTo(0, 0);
     getImage();
-  }
+  };
+
+  const backHome = event => {
+    setIsReading(false);
+  };
 
   const { title, subtitle, message, author } = mainInformation;
   const { articles = [] } = weekInformations || {};
@@ -130,83 +88,75 @@ const App = ({ location }) => {
         <title>{`${htmlTitle || ""} - Equipe.Games`}</title>
         <meta charSet="utf-8" />
       </Helmet>
-      <header className="Header">
-        {isReading && (
-          <button className="ButtonLikeLink Return" onClick={stopReading}>
-            <div className="Chevron Chevron_Left" />
-          </button>
-        )}
-        <a href={url} className="Logo">
-          <img src={Logo} alt="Equipe Games, le meilleur site sportif" />
-        </a>
-      </header>
-
-      {randomImage && (
-        <div className="Articles">
-          <div className="Main">
-            <div
-              className={classnames("Image", {
-                Image_Read: isReading,
-                Image_Loading: isLoading
-              })}
-            >
-              <img
-                src={randomImage}
-                className="MainImage"
-                alt="La folie à la Beaujoire"
-              />
-              <div className="Overlay" />
-              <div className="Text" onClick={readInfo}>
-                <div className="HeadLine">"&nbsp;{title}&nbsp;"</div>
-                <div className="SubHeadLine">"&nbsp;{subtitle}&nbsp;"</div>
-                {!isReading && (
-                  <div className="ChevronContainer">
-                    <div className="Chevron" />
-                  </div>
-                )}
-              </div>
-            </div>
-            {isReading && (
+      <Header url={url} logo={Logo} isHome={!isReading} backHome={backHome} />
+      <div className="Content">
+        {randomImage && (
+          <div className="Articles">
+            <div className="Main">
               <div
-                className={classnames("Article", { Article_Read: isReading })}
+                className={classnames("Image", {
+                  Image_Read: isReading,
+                  Image_Loading: isLoading
+                })}
               >
-                <Lines>{message}</Lines>
-                {author && (
-                  <div className="Author">
-                    {authorIntroduction}
-                    {author}
-                  </div>
-                )}
-              </div>
-            )}
-          </div>
-          <div className="Sides">
-            {articles.map((article, key) => {
-              const { title, subtitle, message } = article || {};
-              if (title === mainInformation.title) return null;
-              return (
-                <div
-                  className="Side"
-                  key={title}
-                  onClick={() => handleReadArticle(article)}
-                >
-                  <h2>{title}</h2>
-                  <h3>{subtitle}</h3>
-                  <p>{message[0].substring(0, 200)}</p>
-                  {(message.length > 1 || message[0].length > 200) && (
-                    <div className="ReadMore">...lire plus</div>
+                <img
+                  src={randomImage}
+                  className="MainImage"
+                  alt="La folie à la Beaujoire"
+                />
+                <div className="Overlay" />
+                <div className="Text" onClick={readInfo}>
+                  <div className="HeadLine">"&nbsp;{title}&nbsp;"</div>
+                  <div className="SubHeadLine">"&nbsp;{subtitle}&nbsp;"</div>
+                  {!isReading && (
+                    <div className="ChevronContainer">
+                      <div className="Chevron" />
+                    </div>
                   )}
                 </div>
-              );
-            })}
+              </div>
+              {isReading && (
+                <div
+                  className={classnames("Article", { Article_Read: isReading })}
+                >
+                  <Lines>{message}</Lines>
+                  {author && (
+                    <div className="Author">
+                      {authorIntroduction}
+                      {author}
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+            <div className="Sides">
+              {articles.map((article, key) => {
+                const { title, subtitle, message } = article || {};
+                if (title === mainInformation.title) return null;
+                return (
+                  <div
+                    className="Side"
+                    key={title}
+                    onClick={() => handleReadArticle(article)}
+                  >
+                    <h2>{title}</h2>
+                    <h3>{subtitle}</h3>
+                    <p>{message[0].substring(0, 200)}</p>
+                    {(message.length > 1 || message[0].length > 200) && (
+                      <div className="ReadMore">...lire plus</div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      )}
-      {!randomImage && (
-        <div className="Loader">
-          Un peu de patience mordu du football, le site charge
-        </div>
-      )}
+        )}
+        {!randomImage && (
+          <div className="Loader">
+            Un peu de patience mordu du football, le site charge
+          </div>
+        )}
+      </div>
     </div>
   );
 };
